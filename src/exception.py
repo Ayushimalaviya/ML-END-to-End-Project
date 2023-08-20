f26c8dd3dfe55b9e35ea97fa10303d977a444ad2
Ayushi Malaviya <ayushimalaviya1999@gmail.com>
2023-08-19 22:18:38 -0400

    Exploratory Analysis

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+# from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error.detail.exc_info()
@@ -16,3 +17,11 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+# if __name__=="__main__":
+
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by zero")
+#         raise CustomException(e,sys)
